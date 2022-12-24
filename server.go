c9008d0dfb7352efb3ce8e2acebcb459573a6f12
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2022-12-24 19:32:01 +0700

    add middleware

--- server.go
+++ server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
