1b4b0ddbe24e6fa8ad12e588e23faab9479be734
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2022-12-24 23:06:46 +0700

    add logic for update

--- handlers/expenses/update.go
+++ handlers/expenses/update.go
@@ -1,9 +1,11 @@
 package expenses
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
 
 func Update(c echo.Context) error {
@@ -11,5 +13,29 @@ func Update(c echo.Context) error {
 	db := open()
 	defer db.Close()
 
-	return c.JSON(http.StatusOK, id)
+	var expense Expense
+
+	queryString := `SELECT * FROM expenses WHERE id=$1`
+	row := db.QueryRow(queryString, id)
+	err := row.Scan(&expense.Id, &expense.Title, &expense.Amount, &expense.Note, pq.Array(&expense.Tags))
+	if err == sql.ErrNoRows {
+		return c.JSON(http.StatusNotFound, "item not found")
+	}
+
+	err = c.Bind(&expense)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	if expense.isEmpty() {
+		return c.JSON(http.StatusBadRequest, "incomplete information")
+	}
+
+	queryString = `UPDATE expenses SET title=$1, amount=$2, note=$3, tags=$4 where id=$5`
+	_, err = db.Exec(queryString, expense.Title, expense.Amount, expense.Note, pq.Array(expense.Tags), expense.Id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, expense)
 }
