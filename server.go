9f8e6570d8f62dd098f9b62e700b18c2ee49acf3
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2022-12-24 19:38:41 +0700

    add initial expenses table with custom middleware

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"os"
 
 	"github.com/labstack/echo/v4"
@@ -13,5 +15,29 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	e.Use(initExpensesTable)
+
 	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
+}
+
+func initExpensesTable(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+		if err != nil {
+			c.Error(err)
+			return err
+		}
+
+		defer db.Close()
+
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS expenses ( id SERIAL PRIMARY KEY, title TEXT, amount FLOAT, note TEXT, tags TEXT[] );`)
+		if err != nil {
+			c.Error(err)
+			return err
+		}
+
+		log.Println("Create successful")
+
+		return next(c)
+	}
 }
