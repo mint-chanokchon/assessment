cbee307d34775e6d4808e53e5717023db0c703b1
Chanokchon Wongjumpa <bike.chanokchon@gmail.com>
2022-12-25 00:28:08 +0700

    add logic for FindAll

--- handlers/expenses/findAll.go
+++ handlers/expenses/findAll.go
@@ -1,11 +1,34 @@
 package expenses
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
 
 func FindAll(c echo.Context) error {
-	return c.JSON(http.StatusOK, nil)
+	db := open()
+	defer db.Close()
+
+	querString := `SELECT * FROM expenses`
+	rows, err := db.Query(querString)
+	if err == sql.ErrNoRows {
+		return c.JSON(http.StatusNotFound, "item not found")
+	}
+
+	var expenses []Expense
+	for rows.Next() {
+		var expense Expense
+
+		err := rows.Scan(&expense.Id, &expense.Title, &expense.Amount, &expense.Note, pq.Array(&expense.Tags))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+
+		expenses = append(expenses, expense)
+	}
+
+	return c.JSON(http.StatusOK, expenses)
 }
